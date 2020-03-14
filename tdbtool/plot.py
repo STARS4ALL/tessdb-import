5a1a5c5db8f81540d8e6e8b86c6423cbc88e3f7d
Rafael Gonzalez <astrorafael@yahoo.es>
2020-03-14 10:17:58 +0100

    plot sketcinhg

--- tdbtool/plot.py
+++ tdbtool/plot.py
@@ -62,6 +62,33 @@ TSTAMP_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
 # Module global functions
 # -----------------------
 
+ef plot1(name):
+	iterable = daily_average_iterable(connection,name)
+	time, median_period = zip(*iterable)
+	median_period = np.array(median_period, dtype=float)
+	fig, axs = plt.subplots(1, 1, tight_layout=True)
+	plt.yscale('log')
+	plt.ylabel('Counts')
+	plt.grid(True)
+	axs.hist(median_period, bins=100)
+	plt.show()
+
+def plot2(name):
+	fig, axs = plt.subplots(1, 2, sharey=True, tight_layout=True)
+	plt.yscale('log')
+	plt.ylabel('Counts')
+	plt.grid(True)
+	plt.title('Histogram of Differnces in time and sequence numbers')
+	min_seq, max_seq, min_sec, max_sec = daily_differences_range(connection, name)
+	iterable = daily_differences_iterable(connection,name)
+	time, seq_diff, seconds_diff = zip(*iterable)
+	seconds_diff = np.array(seconds_diff, dtype=int)
+	seq_diff     = np.array(seq_diff,     dtype=int)
+	print("ARRAYS CREADOS")
+	axs[0].hist(seconds_diff, bins=100)
+	axs[1].hist(seq_diff,     bins=100)
+	plt.show()
+
 
 
 # ==============
